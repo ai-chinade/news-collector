ad140ffe11238c0ff566c1c957f5595c7345c3de
Jing Yuan <jing.yuan@dai-labor.de>
2017-06-06 22:34:47 +0200

    output to file and readme update

--- write_news.py
+++ write_news.py
@@ -1,11 +1,14 @@
 import re
 import subprocess
 import locale
+import sys
 import time
 import json
 from bs4 import BeautifulSoup
 
 locale.setlocale(locale.LC_TIME, "zh_cn")
+news_txt = open('news.txt', 'w')
+sys.stdout = news_txt
 
 TODAY = time.strftime("%Y年%-b月%-d日星期%a")
 TOPK = 5
@@ -16,7 +19,7 @@ TEMPLATE = '『中德人工智能协会』- 今天AI了没？🤣 \n' \
            '关注aichina.de (主页建设中) 成为AI Geek!👽'
 
 subprocess.Popen("bash google-news.sh",  shell=True).communicate()
-print('news json is ready!')
+#print('news json is ready!')
 
 with open('news.json') as fp:
     news = json.load(fp)
@@ -28,3 +31,4 @@ with open('news.json') as fp:
         v['summary'] = re.sub(r'all\s\d+\snews articles', '', v['summary'])
 
 print(TEMPLATE%(TODAY, '\n\n'.join('▶ %s\n%s'%(v['summary'], v['link']) for v in news[:TOPK])))
+news_txt.close()
